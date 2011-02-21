4f210fb7ee35c82f5e3579494e2b36871685d932
Biggerskimo <david.waegner@gmx.de>
2011-02-21 23:36:12 +0100

    Komplettpaket fürs Dilizium eingefügt

--- game-core/src/dilizium.php
+++ game-core/src/dilizium.php
@@ -30,6 +30,9 @@ include($ugamela_root_path . 'includes/planet_toggle.'.$phpEx);
 
 $dpath = (!$user["dpath"]) ? DEFAULT_SKINPATH : $user["dpath"];
 
+if(!defined('ALL')) define('ALL', 320);
+$allList = array('all', 'buildList', 'messageFolders', 'imperium', 'galaxyScans', 'noAds');
+
 if(!defined('BUILDLIST')) define('BUILDLIST', 50);
 if(!defined('MESSAGEFOLDERS')) define('MESSAGEFOLDERS', 40);
 if(!defined('IMPERIUM')) define('IMPERIUM', 20);
@@ -42,69 +45,87 @@ if($_POST) {
 	//$dilizium = $user['dilizium'];
 	//$features = unserialize($user['diliziumFeatures']);
 
-	// build list
-	$buildListDays = abs(intval($_POST['buildList']));
-	$buildListCosts = $buildListDays * BUILDLIST;
+	// all
+	$allDays = abs(intval($_POST['all']));
+	$allCosts = $allDays * ALL;
 
-	if($buildListCosts > $user['dilizium']) $buildListDays = floor($user['dilizium'] / BUILDLIST);
-	$buildListCosts = $buildListDays * BUILDLIST;
-	$user['lostDilizium'] += $buildListCosts;
-	$user['dilizium'] -= $buildListCosts;
+	if($allCosts > $user['dilizium']) $allDays = floor($user['dilizium'] / ALL);
+	$allCosts = $allDays * ALL;
+	$user['lostDilizium'] += $allCosts;
+	$user['dilizium'] -= $allCosts;
 
-	if($features['buildList'] < time()) $features['buildList'] = (time() + $buildListDays * 24 * 60 * 60);
-	else $features['buildList'] += $buildListDays * 24 * 60 * 60;
-
-	// message folders
-	$messageFoldersDays = abs(intval($_POST['messageFolders']));
-	$messageFoldersCosts = $messageFoldersDays * MESSAGEFOLDERS;
-
-	if($messageFoldersCosts > $user['dilizium']) $messageFoldersDays = floor($user['dilizium'] / MESSAGEFOLDERS);
-	$messageFoldersCosts = $messageFoldersDays * MESSAGEFOLDERS;
-	$user['lostDilizium'] += $messageFoldersCosts;
-	$user['dilizium'] -= $messageFoldersCosts;
-
-	if($features['messageFolders'] < time()) $features['messageFolders'] = (time() + $messageFoldersDays * 24 * 60 * 60);
-	else $features['messageFolders'] += $messageFoldersDays * 24 * 60 * 60;
-
-	// imperium
-	$imperiumDays = abs(intval($_POST['imperium']));
-	$imperiumCosts = $imperiumDays * IMPERIUM;
-
-	if($imperiumCosts > $user['dilizium']) $imperiumDays = floor($user['dilizium'] / IMPERIUM);
-	$imperiumCosts = $imperiumDays * IMPERIUM;
-	$user['lostDilizium'] += $imperiumCosts;
-	$user['dilizium'] -= $imperiumCosts;
-
-	if($features['imperium'] < time()) $features['imperium'] = (time() + $imperiumDays * 24 * 60 * 60);
-	else $features['imperium'] += $imperiumDays * 24 * 60 * 60;
-
-	if($imperiumDays) {
-		$page .= '<script type="text/javascript">parent["LeftMenu"].location.reload();</script>';
+	foreach($allList as $elem)
+	{
+		if($features[$elem] < time()) $features[$elem] = (time() + $allDays * 24 * 60 * 60);
+		else $features[$elem] += $allDays * 24 * 60 * 60;
 	}
 
-	// scans in galaxy
-	$galaxyScansDays = abs(intval($_POST['galaxyScans']));
-	$galaxyScansCosts = $galaxyScansDays * GALAXY_SCANS;
+	if(!$allDays)
+	{
+		// build list
+		$buildListDays = abs(intval($_POST['buildList']));
+		$buildListCosts = $buildListDays * BUILDLIST;
 
-	if($galaxyScansCosts > $user['dilizium']) $galaxyScansDays = floor($user['dilizium'] / GALAXY_SCANS);
-	$galaxyScansCosts = $galaxyScansDays * GALAXY_SCANS;
-	$user['lostDilizium'] += $galaxyScansCosts;
-	$user['dilizium'] -= $galaxyScansCosts;
+		if($buildListCosts > $user['dilizium']) $buildListDays = floor($user['dilizium'] / BUILDLIST);
+		$buildListCosts = $buildListDays * BUILDLIST;
+		$user['lostDilizium'] += $buildListCosts;
+		$user['dilizium'] -= $buildListCosts;
 
-	if($features['galaxyScans'] < time()) $features['galaxyScans'] = (time() + $galaxyScansDays * 24 * 60 * 60);
-	else $features['galaxyScans'] += $galaxyScansDays * 24 * 60 * 60;
+		if($features['buildList'] < time()) $features['buildList'] = (time() + $buildListDays * 24 * 60 * 60);
+		else $features['buildList'] += $buildListDays * 24 * 60 * 60;
 
-	// no ads
-	$noAdsDays = abs(intval($_POST['noAds']));
-	$noAdsCosts = $noAdsDays * NO_ADS;
+		// message folders
+		$messageFoldersDays = abs(intval($_POST['messageFolders']));
+		$messageFoldersCosts = $messageFoldersDays * MESSAGEFOLDERS;
 
-	if($noAdsCosts > $user['dilizium']) $noAdsDays = floor($user['dilizium'] / NO_ADS);
-	$noAdsCosts = $noAdsDays * NO_ADS;
-	$user['lostDilizium'] += $noAdsCosts;
-	$user['dilizium'] -= $noAdsCosts;
+		if($messageFoldersCosts > $user['dilizium']) $messageFoldersDays = floor($user['dilizium'] / MESSAGEFOLDERS);
+		$messageFoldersCosts = $messageFoldersDays * MESSAGEFOLDERS;
+		$user['lostDilizium'] += $messageFoldersCosts;
+		$user['dilizium'] -= $messageFoldersCosts;
 
-	if($features['noAds'] < time()) $features['noAds'] = (time() + $noAdsDays * 24 * 60 * 60);
-	else $features['noAds'] += $noAdsDays * 24 * 60 * 60;
+		if($features['messageFolders'] < time()) $features['messageFolders'] = (time() + $messageFoldersDays * 24 * 60 * 60);
+		else $features['messageFolders'] += $messageFoldersDays * 24 * 60 * 60;
+
+		// imperium
+		$imperiumDays = abs(intval($_POST['imperium']));
+		$imperiumCosts = $imperiumDays * IMPERIUM;
+
+		if($imperiumCosts > $user['dilizium']) $imperiumDays = floor($user['dilizium'] / IMPERIUM);
+		$imperiumCosts = $imperiumDays * IMPERIUM;
+		$user['lostDilizium'] += $imperiumCosts;
+		$user['dilizium'] -= $imperiumCosts;
+
+		if($features['imperium'] < time()) $features['imperium'] = (time() + $imperiumDays * 24 * 60 * 60);
+		else $features['imperium'] += $imperiumDays * 24 * 60 * 60;
+
+		if($imperiumDays) {
+			$page .= '<script type="text/javascript">parent["LeftMenu"].location.reload();</script>';
+		}
+
+		// scans in galaxy
+		$galaxyScansDays = abs(intval($_POST['galaxyScans']));
+		$galaxyScansCosts = $galaxyScansDays * GALAXY_SCANS;
+
+		if($galaxyScansCosts > $user['dilizium']) $galaxyScansDays = floor($user['dilizium'] / GALAXY_SCANS);
+		$galaxyScansCosts = $galaxyScansDays * GALAXY_SCANS;
+		$user['lostDilizium'] += $galaxyScansCosts;
+		$user['dilizium'] -= $galaxyScansCosts;
+
+		if($features['galaxyScans'] < time()) $features['galaxyScans'] = (time() + $galaxyScansDays * 24 * 60 * 60);
+		else $features['galaxyScans'] += $galaxyScansDays * 24 * 60 * 60;
+
+		// no ads
+		$noAdsDays = abs(intval($_POST['noAds']));
+		$noAdsCosts = $noAdsDays * NO_ADS;
+
+		if($noAdsCosts > $user['dilizium']) $noAdsDays = floor($user['dilizium'] / NO_ADS);
+		$noAdsCosts = $noAdsDays * NO_ADS;
+		$user['lostDilizium'] += $noAdsCosts;
+		$user['dilizium'] -= $noAdsCosts;
+
+		if($features['noAds'] < time()) $features['noAds'] = (time() + $noAdsDays * 24 * 60 * 60);
+		else $features['noAds'] += $noAdsDays * 24 * 60 * 60;
+	}
 
 	// save
 	$user['diliziumFeatures'] = serialize($features);
@@ -118,17 +139,41 @@ if($_POST) {
 	WCF::getSession()->setUpdate(true);
 }
 
-$page .= '<script type="text/javascript">
+$page .= '<script type="text/javascript" src="game/js/jq.js"></script>
+		<script type="text/javascript">
 			var costsM = 0;
 			function calculateCosts(container) {
 				var old = document.getElementById(container + "C").firstChild.data;
 				var days = document.getElementById(container + "V").value;
+				var costsPerDay = document.getElementById(container + "D").firstChild.data;
+				var costs = days * costsPerDay;
 				if(days < 0) {
 					document.getElementById(container + "V").value = 0;
 					days = 0;
 				}
-				var costsPerDay = document.getElementById(container + "D").firstChild.data;
-				var costs = days * costsPerDay;
+				if(container == "all")
+				{
+					if(days > 0)
+					{
+						$(".notAll input").attr("disabled", "disabled");
+						$(".notAll").addClass("disabled");
+						
+						// skip overall costs
+						document.getElementById("costs").firstChild.data = costs;
+						document.getElementById(container + "C").firstChild.data = costs;
+						
+						if(costs > document.getElementById("dilizium").firstChild.data) document.getElementById("costs").style.color = "red";
+						else document.getElementById("costs").style.color = "";
+						
+						return;
+					}
+					else
+					{
+						$(".notAll input").removeAttr("disabled");
+						$(".notAll").removeClass("disabled");
+						old = 0;
+					}
+				}
 				document.getElementById(container + "C").firstChild.data = costs;
 				var diff = costs - old;
 				costsM += diff;
@@ -146,10 +191,22 @@ $page .= '<tr>';
 $page .= '<td class="l" colspan="2">Aktueller Dilizium-Vorrat:</td>';
 $page .= '<td class="l" colspan="2"><span id="dilizium">'.$user['dilizium'].'</span></td>';
 $page .= '</tr>';
+if(WCF::getUser()->userID == 1) {
+$page .= '<tr>';
+$page .= '<td class="c" colspan="4">Bekommen</td>';
+$page .= '</tr>';
+$page .= '<tr>';
+$page .= '<td colspan="4" style="text-align: center;"><a href="game/index.php?page=Payment"><span style="color: gold;">Kaufen</span></a></td>';
+$page .= '</tr>';
+$page .= '<tr>';
+$page .= '<td colspan="4" style="text-align: center;">Andere  M&ouml;glichkeiten: Forum und (das ist zur Zeit leider nur geplant) im Chat.</td>';
+$page .= '</tr>';
+}
+/*
 $page .= '<tr>';
 $page .= '<td class="l" colspan="2">Dein Werbe-Link:</td>';
 $page .= '<td class="l" colspan="2"><input name="linktext" type="text" style="width: 96%;" value="http://lost-worlds.net/ref.php?u='.WCF::getUser()->userID.'" onClick="this.form.linktext.select(); this.form.linktext.focus();" /></td>';
-$page .= '</tr>';
+$page .= '</tr>';*/
 $page .= '<tr>';
 $page .= '<td class="c" colspan="4">Verwenden</td>';
 $page .= '</tr>';
@@ -159,28 +216,37 @@ $page .= '<td class="c">Kosten/Tag</td>';
 $page .= '<td class="c">Tage</td>';
 $page .= '<td class="c">Effektive Kosten</td>';
 $page .= '</tr>';
+if(WCF::getUser()->userID == 1) {
 $page .= '<tr>';
+if($features['buildList'] > time()) $page .= '<td class="l"><span title="Noch ca. '.round(($features['buildList'] - time()) / (24 * 60 * 60)).' Tag(e) aktiviert">Komplett-Paket (ca. 40% billiger!)</span></td>';
+else $page .= '<td class="l"><span title="Nicht aktiviert">Komplett-Paket (ca. 40% billiger!)</span></td>';
+$page .= '<td class="l"><span id="allD">'.ALL.'</span></td>';
+$page .= '<td class="l"><input id="allV" name="all" type="text" size="3" maxlength="2" onKeyUp="calculateCosts(\'all\');" /></td>';
+$page .= '<td class="l"><span id="allC">0</span></td>';
+$page .= '</tr>';
+}
+$page .= '<tr class="notAll">';
 if($features['buildList'] > time()) $page .= '<td class="l"><span title="Noch ca. '.round(($features['buildList'] - time()) / (24 * 60 * 60)).' Tag(e) aktiviert">Bauliste</span></td>';
 else $page .= '<td class="l"><span title="Nicht aktiviert">Bauliste</span></td>';
 $page .= '<td class="l"><span id="buildListD">'.BUILDLIST.'</span></td>';
 $page .= '<td class="l"><input id="buildListV" name="buildList" type="text" size="3" maxlength="2" onKeyUp="calculateCosts(\'buildList\');" /></td>';
 $page .= '<td class="l"><span id="buildListC">0</span></td>';
 $page .= '</tr>';
-$page .= '<tr>';
+$page .= '<tr class="notAll">';
 if($features['messageFolders'] > time()) $page .= '<td class="l"><span title="Noch ca. '.round(($features['messageFolders'] - time()) / (24 * 60 * 60)).' Tag(e) aktiviert">Nachrichten-Ordner</span></td>';
 else $page .= '<td class="l"><span title="Nicht aktiviert">Nachrichten-Ordner</span></td>';
 $page .= '<td class="l"><span id="messageFoldersD">'.MESSAGEFOLDERS.'</span></td>';
 $page .= '<td class="l"><input id="messageFoldersV" name="messageFolders" type="text" size="3" maxlength="2" onKeyUp="calculateCosts(\'messageFolders\');" /></td>';
 $page .= '<td class="l"><span id="messageFoldersC">0</span></td>';
 $page .= '</tr>';
-$page .= '<tr>';
+$page .= '<tr class="notAll">';
 if($features['imperium'] > time()) $page .= '<td class="l"><span title="Noch ca. '.round(($features['imperium'] - time()) / (24 * 60 * 60)).' Tag(e) aktiviert">Imperiums-Ansicht</span></td>';
 else $page .= '<td class="l"><span title="Nicht aktiviert">Imperiums-Ansicht</span></td>';
 $page .= '<td class="l"><span id="imperiumD">'.IMPERIUM.'</span></td>';
 $page .= '<td class="l"><input id="imperiumV" name="imperium" type="text" size="3" maxlength="2" onKeyUp="calculateCosts(\'imperium\');" /></td>';
 $page .= '<td class="l"><span id="imperiumC">0</span></td>';
 $page .= '</tr>';
-$page .= '<tr>';
+$page .= '<tr class="notAll">';
 if($features['galaxyScans'] > time()) $page .= '<td class="l"><span title="Noch ca. '.round(($features['galaxyScans'] - time()) / (24 * 60 * 60)).' Tag(e) aktiviert">Spionageberichte in Galaxie-Ansicht</span></td>';
 else $page .= '<td class="l"><span title="Nicht aktiviert">Spionageberichte in Galaxie-Ansicht</span></td>';
 $page .= '<td class="l"><span id="galaxyScansD">'.GALAXY_SCANS.'</span></td>';
